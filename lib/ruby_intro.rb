3df25df530e5a759b1bb768b44318d727ce893f1
cerodav <sherinedavis@outlook.com>
2021-09-13 03:37:30 -0500

    First Take

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,34 +2,94 @@
 
 # Part 1
 
-def sum arr
-  # YOUR CODE HERE
+def sum(arr)
+  arrLength = arr.length()
+  counter = 0
+  sum = 0
+  while counter < arrLength 
+    sum += arr[counter]
+    counter += 1
+  end
+  return sum
 end
 
-def max_2_sum arr
-  # YOUR CODE HERE
+def max_2_sum(arr)
+  if arr.length() == 0
+    return 0
+  end
+  if arr.length() == 1
+    return arr[0]
+  end
+  arr = arr.sort()
+  lastTwo = arr.last(2)
+  return lastTwo[0] + lastTwo[1]
 end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
+def sum_to_n?(arr, n)
+  itemMap = {}
+  arrLength = arr.length()
+  
+  if arrLength <= 2
+    return false
+  end
+
+  counter = 0
+  while counter < arrLength
+    val = arr[counter]
+    counter += 1
+    if itemMap.key?(n-val)
+      return true
+    end
+    itemMap[val] = true
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, #{name}"
 end
 
-def starts_with_consonant? s
-  # YOUR CODE HERE
+def starts_with_consonant?(s)
+  strLength = s.length()
+  vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+  if strLength > 0
+    char = s[0]
+    puts "The string is #{s} and char is #{char}"
+    if vowels.include?(char) or !char.match?(/[A-Za-z]/)
+      return false
+    end
+    return true
+  end
+  return false
 end
 
-def binary_multiple_of_4? s
-  # YOUR CODE HERE
+def binary_multiple_of_4?(s)
+  if s.length() == 0 or !s.match?(/^[01]+$/)
+    return false
+  end
+  if s.to_i(2)%4 == 0
+    return true
+  end
+  return false
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn, cost)
+    if isbn.length == 0 or cost <= 0
+      raise ArgumentError.new("Invalid Input")
+    end
+    @isbn = isbn
+    @price = cost
+  end
+
+  def price_as_string()
+    return "#{'$%.2f' % @price}"
+  end
+
+  attr_accessor :price
+  attr_accessor :isbn
 end
